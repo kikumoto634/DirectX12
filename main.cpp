ddb898a898e8005da4ca7e1f702c1447c5e1eac5
634_3 <k020g1146@g.neec.ac.jp>
2022-05-27 23:36:28 +0900

    メッセージループ

--- main.cpp
+++ main.cpp
@@ -19,5 +19,85 @@ LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 /// Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE,LPSTR,int)
 {
+	//ウィンドウサイズ
+	const int window_width = 1280;	//横幅
+	const int window_height = 720;	//縦幅
+
+	//ウィンドウクラス設定
+	WNDCLASSEX w{};
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProc;		//ウィンドウプロシージャ設定
+	w.lpszClassName = L"DirectXGame";			//ウィンドウクラス名
+	w.hInstance = GetModuleHandle(nullptr);		//ウィンドウハンドル
+	w.hCursor = LoadCursor(NULL, IDC_ARROW);	//カーソル指定
+
+	//ウィンドウクラスをOSに登録する
+	RegisterClassEx(&w);
+	//ウィンドウサイズ{ X座標 Y座標 横幅 縦幅}
+	RECT wrc = {0, 0, window_width, window_height};
+	//自動でサイズを補正する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	///ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindow(
+		w.lpszClassName,			//クラス名
+		L"DirectXGame",				//タイトルバー文字
+		WS_OVERLAPPEDWINDOW,		//標準的なウィンドウスタイル
+		CW_USEDEFAULT,				//表示X座標(OSに任せる)
+		CW_USEDEFAULT,				//表示Y座標(OSに任せる)
+		wrc.right  - wrc.left,		//ウィンドウ横幅
+		wrc.bottom - wrc.top,		//ウィンドウ縦幅
+		nullptr,					//親ウィンドウハンドル
+		nullptr,					//メニューハンドル
+		w.hInstance,				//呼び出しアプリケーションハンドル
+		nullptr						//オプション
+	);
+	//ウィンドウを表示状態にする
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg{};	//メッセージ
+
+
+	/// <summary>
+	/// DirectX12 初期化処理 ここから
+	/// </summary>
+
+
+
+	/// <summary>
+	/// DirectX12 初期化処理 ここまで
+	/// </summary>
 	
+	/// <summary>
+	/// ゲームループ
+	/// </summary>
+	while(true)
+	{
+		//メッセージがある?
+		if(PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);	//キー入力メッセージの処理
+			DispatchMessage(&msg);	//プロシージャにメッセージを送る
+		}
+
+		//×ボタンで終了メッセージが来たらゲームループを抜ける
+		if(msg.message == WM_QUIT)
+		{
+			break;
+		}
+
+
+		/// <summary>
+		/// DirectX12 毎フレーム処理 ここから
+		/// </summary>
+
+
+
+		/// <summary>
+		/// DirectX12 毎フレーム処理 ここまで
+		/// </summary>
+
+	}
+
+	///ウィンドウクラスを登録解除
 }
