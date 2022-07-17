0ee96fbfdd7a423ba9395ab3f34c8bca11a567ec
634_3 <k020g1146@g.neec.ac.jp>
2022-07-17 22:52:12 +0900

    幾何学クラス設計中

---
+++ GeometryModel.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <DirectXMath.h>
+#include <d3d12.h>
+
+#include "DirectXCommon.h"
+
+/// <summary>
+/// 幾何学モデル
+/// </summary>
+class GeometryModel
+{
+/// <summary>
+/// インナークラス
+/// </summary>
+public:
+	//頂点データ構造体(3D)
+	struct Vertex
+	{
+		DirectX::XMFLOAT3 pos;	//xyz座標
+		DirectX::XMFLOAT3 normal;//法線ベクトル
+		DirectX::XMFLOAT2 uv;	//uv座標
+	};
+
+/// <summary>
+/// メンバ関数
+/// </summary>
+public:
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize(DirectXCommon* dxCommon);
+};
+
