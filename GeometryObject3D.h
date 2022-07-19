41e4b72f171ef854e7983edd282bf1b1063bc7d2
k020g1146 <k020g1146@g.neec.ac.jp>
2022-07-19 16:12:15 +0900

    GeometryModel 共通グラフィックスパイプラインの生成

---
+++ GeometryObject3D.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+#include <wrl.h>
+#include <d3d12.h>
+
+#include "DirectXCommon.h"
+#include "GeometryModel.h"
+
+/// <summary>
+/// 数式で生成したモデル用3Dオブジェクト
+/// </summary>
+class GeometryObject3D
+{
+/// <summary>
+/// エイリアス
+/// </summary>
+public:
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+/// <summary>
+/// インナークラス
+/// </summary>
+public:
+
+	//共通データ
+	class Common
+	{
+		friend class GeometryObject3D;
+		
+	public:
+		/// <summary>
+		/// グラフィックスパイプライン初期化
+		/// </summary>
+		void InitializeGraphicsPipeline(DirectXCommon* dxCommon);
+
+	private:
+		//パイプラインステートオブジェクト
+		ComPtr<ID3D12PipelineState> pipelinestate;
+		//ルートシグネチャ
+		ComPtr<ID3D12RootSignature> rootsignature;
+
+		//モデルデータ
+		GeometryModel* model = nullptr;
+	};
+
+/// <summary>
+/// 静的メンバ関数
+/// </summary>
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	static void StaticInitialize(DirectXCommon* dxCommon, GeometryModel* model);
+
+	/// <summary>
+	/// 解放
+	/// </summary>
+	static void StaticFinalize();
+
+/// <summary>
+/// 静的メンバ変数
+/// </summary>
+private:
+	static Common* common;
+
+/// <summary>
+/// メンバ関数
+/// </summary>
+public:
+	void Initialize();
+
+private:
+};
+
--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 #include <cassert>
 ////図形描画
 #include "GeometryModel.h"
-//D3Dコンパイラのインクルード
-#include <d3dcompiler.h>
+#include "GeometryObject3D.h"
 //キーボード入力
 #include "Input.h"
 //DirectXTex導入
@@ -22,8 +21,8 @@
 //デバックテキスト
 #include "DebugText.h"
 
-//音声
-#pragma comment(lib, "d3dcompiler.lib")
+////音声
+//#pragma comment(lib, "d3dcompiler.lib")
 
 using namespace DirectX;
 using namespace Microsoft::WRL;
@@ -57,18 +56,18 @@ struct Object3d
 	Object3d* parent = nullptr;
 };
 
-//パイプラインセット
-struct PipelineSet
-{
-	//パイプラインステートオブジェクト
-	ComPtr<ID3D12PipelineState> pipelinestate;
-	//ルートシグネチャ
-	ComPtr<ID3D12RootSignature> rootsignature;
-};
+////パイプラインセット
+//struct PipelineSet
+//{
+//	////パイプラインステートオブジェクト
+//	//ComPtr<ID3D12PipelineState> pipelinestate;
+//	////ルートシグネチャ
+//	//ComPtr<ID3D12RootSignature> rootsignature;
+//};
 
 
 //3Dオブジェクト用パイプライン生成
-PipelineSet Object3dCreateGraphicsPipeline(ID3D12Device* device);
+//PipelineSet Object3dCreateGraphicsPipeline(ID3D12Device* device);
 
 //3D共通グラフィックスコマンドのセット
 void Object3DCommonBeginDraw(ID3D12GraphicsCommandList* commandList, const PipelineSet& pipelineSet);
@@ -128,7 +127,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE,LPSTR,int)
 	/// <summary>
 	/// 初期化変数
 	/// </summary>
-	HRESULT result;
 
 	//スプライト共通テクスチャ読込
 	textureManager->LoadTexture(0, L"Resources/Texture.jpg");
@@ -164,6 +162,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE,LPSTR,int)
 	////スプライト共通データ生成
 	Sprite::StaticInitialize(dxCommon, textureManager);
 
+	///幾何学オブジェクト共通でーた生成
+	GeometryObject3D::StaticInitialize(dxCommon, geometryModel);
 
 	//デバックテキスト世のテクスチャ番号
 	const int debugTextTexNumber = 2;
@@ -190,15 +190,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE,LPSTR,int)
 	sprite2->SetTextureRect(0.f, 0.f, 400.f, 400.f);
 
 	//3Dオブジェクト用パイプライン生成
-	PipelineSet object3dPipelineSet = Object3dCreateGraphicsPipeline(dxCommon->GetDevice());
+	//PipelineSet object3dPipelineSet = Object3dCreateGraphicsPipeline(dxCommon->GetDevice());
 
 
 	//定数バッファ Mat	
 	//透視投影
 	XMMATRIX matProjection;
 	//ビュー変換行列
+	float angle = 0.0f;//カメラの回転角
+	float distance = -100.f;	//カメラの距離
 	XMMATRIX matView;
-	XMFLOAT3 eye = {0.0f, 0.0f, -100.0f};	//視点座標
+	XMFLOAT3 eye = {0.0f, 0.0f, distance};	//視点座標
 	XMFLOAT3 target= {0, 0, 0};//注視点座標
 	XMFLOAT3 up = {0, 1, 0};	//上方向ベクトル
 
@@ -248,8 +250,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE,LPSTR,int)
 	/// DirectX12 描画初期化処理 ここまで
 	/// </summary>
 	 
-
-	float angle = 0.0f;//カメラの回転角
 
 
 	/// <summary>
@@ -369,6 +369,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE,LPSTR,int)
 	delete debugText;
 	delete sprite;
 	delete sprite2;
+	GeometryObject3D::StaticFinalize();
 	Sprite::StaticFinalize();
 	delete input;
 	delete soundManager;
@@ -384,181 +385,181 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE,LPSTR,int)
 
 
 //3Dオブジェクト用パイプラインセット
-PipelineSet Object3dCreateGraphicsPipeline(ID3D12Device* device)
-{
-	HRESULT result;
+//PipelineSet Object3dCreateGraphicsPipeline(ID3D12Device* device)
+//{
+	//HRESULT result;
 
-	///頂点シェーダーfileの読み込みとコンパイル
-	ComPtr<ID3DBlob> vsBlob ;			//頂点シェーダーオブジェクト
-	ComPtr<ID3DBlob> psBlob ;			//ピクセルシェーダーオブジェクト
-	ComPtr<ID3DBlob> errorBlob ;		//エラーオブジェクト
+	/////頂点シェーダーfileの読み込みとコンパイル
+	//ComPtr<ID3DBlob> vsBlob ;			//頂点シェーダーオブジェクト
+	//ComPtr<ID3DBlob> psBlob ;			//ピクセルシェーダーオブジェクト
+	//ComPtr<ID3DBlob> errorBlob ;		//エラーオブジェクト
 
-	//頂点シェーダーの読み込みコンパイル
-	result = D3DCompileFromFile(
-		L"BasicVS.hlsl",		//シェーダーファイル名
-		nullptr,
-		D3D_COMPILE_STANDARD_FILE_INCLUDE,	//インクルード可能にする
-		"main", "vs_5_0",					//エントリーポイント名、シェーダーモデル指定
-		D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,	//デバック用設定
-		0,
-		&vsBlob, &errorBlob);
-	//エラーなら
-	if(FAILED(result)){
-		//errorBlobからエラー内容をstring型にコピー
-		std::string error;
-		error.resize(errorBlob->GetBufferSize());
+	////頂点シェーダーの読み込みコンパイル
+	//result = D3DCompileFromFile(
+	//	L"BasicVS.hlsl",		//シェーダーファイル名
+	//	nullptr,
+	//	D3D_COMPILE_STANDARD_FILE_INCLUDE,	//インクルード可能にする
+	//	"main", "vs_5_0",					//エントリーポイント名、シェーダーモデル指定
+	//	D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,	//デバック用設定
+	//	0,
+	//	&vsBlob, &errorBlob);
+	////エラーなら
+	//if(FAILED(result)){
+	//	//errorBlobからエラー内容をstring型にコピー
+	//	std::string error;
+	//	error.resize(errorBlob->GetBufferSize());
 
-		std::copy_n((char*)errorBlob->GetBufferPointer(),
-					errorBlob->GetBufferSize(),
-					error.begin());
-		error += "\n";
-		//エラー内容を出力ウィンドウに表示
-		OutputDebugStringA(error.c_str());
-		assert(0);
-	}
+	//	std::copy_n((char*)errorBlob->GetBufferPointer(),
+	//				errorBlob->GetBufferSize(),
+	//				error.begin());
+	//	error += "\n";
+	//	//エラー内容を出力ウィンドウに表示
+	//	OutputDebugStringA(error.c_str());
+	//	assert(0);
+	//}
 
-	//ピクセルシェーダーの読み込みコンパイル
-	result = D3DCompileFromFile(
-		L"BasicPS.hlsl",		//シェーダーファイル名
-		nullptr,
-		D3D_COMPILE_STANDARD_FILE_INCLUDE,	//インクルード可能にする
-		"main", "ps_5_0",					//エントリーポイント名、シェーダーモデル指定
-		D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,	//デバック用設定
-		0,
-		&psBlob, &errorBlob);
-	//エラーなら
-	if(FAILED(result)){
-		//errorBlobからエラー内容をstring型にコピー
-		std::string error;
-		error.resize(errorBlob->GetBufferSize());
+	////ピクセルシェーダーの読み込みコンパイル
+	//result = D3DCompileFromFile(
+	//	L"BasicPS.hlsl",		//シェーダーファイル名
+	//	nullptr,
+	//	D3D_COMPILE_STANDARD_FILE_INCLUDE,	//インクルード可能にする
+	//	"main", "ps_5_0",					//エントリーポイント名、シェーダーモデル指定
+	//	D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,	//デバック用設定
+	//	0,
+	//	&psBlob, &errorBlob);
+	////エラーなら
+	//if(FAILED(result)){
+	//	//errorBlobからエラー内容をstring型にコピー
+	//	std::string error;
+	//	error.resize(errorBlob->GetBufferSize());
 
-		std::copy_n((char*)errorBlob->GetBufferPointer(),
-					errorBlob->GetBufferSize(),
-					error.begin());
-		error += "\n";
-		//エラー内容を出力ウィンドウに表示
-		OutputDebugStringA(error.c_str());
-		assert(0);
-	}
-
-
-	///頂点レイアウト
-	D3D12_INPUT_ELEMENT_DESC inputLayout[] = {
-	
-		{//xyz座標
-			"POSITION",										//セマンティック名
-			0,												//同じセマンティック名が複数あるときに使うインデックス
-			DXGI_FORMAT_R32G32B32_FLOAT,					//要素数とビット数を表す (XYZの3つでfloat型なのでR32G32B32_FLOAT)
-			0,												//入力スロットインデックス
-			D3D12_APPEND_ALIGNED_ELEMENT,					//データのオフセット値 (D3D12_APPEND_ALIGNED_ELEMENTだと自動設定)
-			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,		//入力データ種別 (標準はD3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA)
-			0												//一度に描画するインスタンス数
-		},
-		{//法線ベクトル
-			"NORMAL",
-			0,
-			DXGI_FORMAT_R32G32B32_FLOAT,
-			0,
-			D3D12_APPEND_ALIGNED_ELEMENT,
-			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,
-			0
-		},
-		{//uv座標
-			"TEXCOORD",
-			0,
-			DXGI_FORMAT_R32G32_FLOAT,
-			0,
-			D3D12_APPEND_ALIGNED_ELEMENT,
-			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,
-			0
-		},
-	};
-
-	///ルートパラメータ
-	//デスクリプタレンジの設定
-	CD3DX12_DESCRIPTOR_RANGE descRangeSRV{};
-	descRangeSRV.Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0);
+	//	std::copy_n((char*)errorBlob->GetBufferPointer(),
+	//				errorBlob->GetBufferSize(),
+	//				error.begin());
+	//	error += "\n";
+	//	//エラー内容を出力ウィンドウに表示
+	//	OutputDebugStringA(error.c_str());
+	//	assert(0);
+	//}
 
 
-	//設定
-	////定数バッファ 0番
-	CD3DX12_ROOT_PARAMETER rootParam[2] = {};
-	////定数　0番 material
-	rootParam[0].InitAsConstantBufferView(0);
-	////テクスチャレジスタ 0番
-	rootParam[1].InitAsDescriptorTable(1, &descRangeSRV);
+	/////頂点レイアウト
+	//D3D12_INPUT_ELEMENT_DESC inputLayout[] = {
+	//
+	//	{//xyz座標
+	//		"POSITION",										//セマンティック名
+	//		0,												//同じセマンティック名が複数あるときに使うインデックス
+	//		DXGI_FORMAT_R32G32B32_FLOAT,					//要素数とビット数を表す (XYZの3つでfloat型なのでR32G32B32_FLOAT)
+	//		0,												//入力スロットインデックス
+	//		D3D12_APPEND_ALIGNED_ELEMENT,					//データのオフセット値 (D3D12_APPEND_ALIGNED_ELEMENTだと自動設定)
+	//		D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,		//入力データ種別 (標準はD3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA)
+	//		0												//一度に描画するインスタンス数
+	//	},
+	//	{//法線ベクトル
+	//		"NORMAL",
+	//		0,
+	//		DXGI_FORMAT_R32G32B32_FLOAT,
+	//		0,
+	//		D3D12_APPEND_ALIGNED_ELEMENT,
+	//		D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,
+	//		0
+	//	},
+	//	{//uv座標
+	//		"TEXCOORD",
+	//		0,
+	//		DXGI_FORMAT_R32G32_FLOAT,
+	//		0,
+	//		D3D12_APPEND_ALIGNED_ELEMENT,
+	//		D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,
+	//		0
+	//	},
+	//};
+
+	/////ルートパラメータ
+	////デスクリプタレンジの設定
+	//CD3DX12_DESCRIPTOR_RANGE descRangeSRV{};
+	//descRangeSRV.Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0);
 
 
-	///<summmary>
-	///グラフィックスパイプライン
-	///<summary/>
-	
-	//グラフィックスパイプライン設定
-	D3D12_GRAPHICS_PIPELINE_STATE_DESC pipelineDesc{};
-	//シェーダー設定
-	pipelineDesc.VS = CD3DX12_SHADER_BYTECODE(vsBlob.Get());
-	pipelineDesc.PS = CD3DX12_SHADER_BYTECODE(psBlob.Get());
-	
-	//サンプルマスク設定
-	pipelineDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;	//標準設定
-	//ラスタライザ設定 背面カリング	ポリゴン内塗りつぶし	深度クリッピング有効
-	pipelineDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
-	//ブレンドステート
-	//レンダーターゲットのブレンド設定
-	D3D12_RENDER_TARGET_BLEND_DESC& blenddesc = pipelineDesc.BlendState.RenderTarget[0];
-	blenddesc.RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;	//RGBAすべてのチャンネルを描画
-	//共通設定
-	blenddesc.BlendEnable = true;						//ブレンドを有効にする
-	blenddesc.BlendOpAlpha = D3D12_BLEND_OP_ADD;		//加算
-	blenddesc.SrcBlendAlpha = D3D12_BLEND_ONE;			//ソースの値を100% 使う	(ソースカラー			 ： 今から描画しようとしている色)
-	blenddesc.DestBlendAlpha = D3D12_BLEND_ZERO;		//デストの値を  0% 使う	(デスティネーションカラー： 既にキャンバスに描かれている色)
-	//各種設定
-	blenddesc.BlendOp = D3D12_BLEND_OP_ADD;	//設定
-	blenddesc.SrcBlend = D3D12_BLEND_SRC_ALPHA;			//ソースの値を 何% 使う
-	blenddesc.DestBlend = D3D12_BLEND_INV_SRC_ALPHA;	//デストの値を 何% 使う
-	//頂点レイアウト設定
-	pipelineDesc.InputLayout.pInputElementDescs = inputLayout;
-	pipelineDesc.InputLayout.NumElements = _countof(inputLayout);
-	//図形の形状設定 (プリミティブトポロジー)
-	pipelineDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
-	//その他設定
-	pipelineDesc.NumRenderTargets = 1;		//描画対象は一つ
-	pipelineDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;	//0~255指定のRGBA
-	pipelineDesc.SampleDesc.Count = 1;	//1ピクセルにつき1回サンプリング
-	//デプスステンシルステートの設定	(深度テストを行う、書き込み許可、深度がちいさければ許可)
-	pipelineDesc.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
-	pipelineDesc.DSVFormat = DXGI_FORMAT_D32_FLOAT;	//深度値フォーマット
-
-	///テクスチャサンプラー
-	//設定
-	CD3DX12_STATIC_SAMPLER_DESC samplerDesc = CD3DX12_STATIC_SAMPLER_DESC(0);
+	////設定
+	//////定数バッファ 0番
+	//CD3DX12_ROOT_PARAMETER rootParam[2] = {};
+	//////定数　0番 material
+	//rootParam[0].InitAsConstantBufferView(0);
+	//////テクスチャレジスタ 0番
+	//rootParam[1].InitAsDescriptorTable(1, &descRangeSRV);
 
 
-	//パイプラインとルートシグネチャのセット
-	PipelineSet pipelineSet;
+	/////<summmary>
+	/////グラフィックスパイプライン
+	/////<summary/>
+	//
+	////グラフィックスパイプライン設定
+	//D3D12_GRAPHICS_PIPELINE_STATE_DESC pipelineDesc{};
+	////シェーダー設定
+	//pipelineDesc.VS = CD3DX12_SHADER_BYTECODE(vsBlob.Get());
+	//pipelineDesc.PS = CD3DX12_SHADER_BYTECODE(psBlob.Get());
+	//
+	////サンプルマスク設定
+	//pipelineDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;	//標準設定
+	////ラスタライザ設定 背面カリング	ポリゴン内塗りつぶし	深度クリッピング有効
+	//pipelineDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+	////ブレンドステート
+	////レンダーターゲットのブレンド設定
+	//D3D12_RENDER_TARGET_BLEND_DESC& blenddesc = pipelineDesc.BlendState.RenderTarget[0];
+	//blenddesc.RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;	//RGBAすべてのチャンネルを描画
+	////共通設定
+	//blenddesc.BlendEnable = true;						//ブレンドを有効にする
+	//blenddesc.BlendOpAlpha = D3D12_BLEND_OP_ADD;		//加算
+	//blenddesc.SrcBlendAlpha = D3D12_BLEND_ONE;			//ソースの値を100% 使う	(ソースカラー			 ： 今から描画しようとしている色)
+	//blenddesc.DestBlendAlpha = D3D12_BLEND_ZERO;		//デストの値を  0% 使う	(デスティネーションカラー： 既にキャンバスに描かれている色)
+	////各種設定
+	//blenddesc.BlendOp = D3D12_BLEND_OP_ADD;	//設定
+	//blenddesc.SrcBlend = D3D12_BLEND_SRC_ALPHA;			//ソースの値を 何% 使う
+	//blenddesc.DestBlend = D3D12_BLEND_INV_SRC_ALPHA;	//デストの値を 何% 使う
+	////頂点レイアウト設定
+	//pipelineDesc.InputLayout.pInputElementDescs = inputLayout;
+	//pipelineDesc.InputLayout.NumElements = _countof(inputLayout);
+	////図形の形状設定 (プリミティブトポロジー)
+	//pipelineDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	////その他設定
+	//pipelineDesc.NumRenderTargets = 1;		//描画対象は一つ
+	//pipelineDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;	//0~255指定のRGBA
+	//pipelineDesc.SampleDesc.Count = 1;	//1ピクセルにつき1回サンプリング
+	////デプスステンシルステートの設定	(深度テストを行う、書き込み許可、深度がちいさければ許可)
+	//pipelineDesc.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
+	//pipelineDesc.DSVFormat = DXGI_FORMAT_D32_FLOAT;	//深度値フォーマット
+
+	/////テクスチャサンプラー
+	////設定
+	//CD3DX12_STATIC_SAMPLER_DESC samplerDesc = CD3DX12_STATIC_SAMPLER_DESC(0);
 
 
-	//ルートシグネチャ (テクスチャ、定数バッファなどシェーダーに渡すリソース情報をまとめたオブジェクト)
-	//設定
-	CD3DX12_VERSIONED_ROOT_SIGNATURE_DESC rootSignatureDesc{};
-	rootSignatureDesc.Init_1_0(_countof(rootParam), rootParam,1, &samplerDesc,D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
-	//シリアライズ
-	ComPtr<ID3DBlob> rootSigBlob;
-	result = D3DX12SerializeVersionedRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1_0, &rootSigBlob,&errorBlob);
-	assert(SUCCEEDED(result));
-	result = dxCommon->GetDevice()->CreateRootSignature(0,rootSigBlob->GetBufferPointer(), rootSigBlob->GetBufferSize(),IID_PPV_ARGS(&pipelineSet.rootsignature));
-	assert(SUCCEEDED(result));
-	//パイプラインにルートシグネチャをセット
-	pipelineDesc.pRootSignature = pipelineSet.rootsignature.Get();
+	////パイプラインとルートシグネチャのセット
+	//PipelineSet pipelineSet;
 
-	//パイプラインステート (グラフィックスパイプラインの設定をまとめたのがパイプラインステートオブジェクト(PSO))
-	//パイプラインステートの生成
-	result = dxCommon->GetDevice()->CreateGraphicsPipelineState(&pipelineDesc, IID_PPV_ARGS(&pipelineSet.pipelinestate));
-	assert(SUCCEEDED(result));
+
+	////ルートシグネチャ (テクスチャ、定数バッファなどシェーダーに渡すリソース情報をまとめたオブジェクト)
+	////設定
+	//CD3DX12_VERSIONED_ROOT_SIGNATURE_DESC rootSignatureDesc{};
+	//rootSignatureDesc.Init_1_0(_countof(rootParam), rootParam,1, &samplerDesc,D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+	////シリアライズ
+	//ComPtr<ID3DBlob> rootSigBlob;
+	//result = D3DX12SerializeVersionedRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1_0, &rootSigBlob,&errorBlob);
+	//assert(SUCCEEDED(result));
+	//result = dxCommon->GetDevice()->CreateRootSignature(0,rootSigBlob->GetBufferPointer(), rootSigBlob->GetBufferSize(),IID_PPV_ARGS(&pipelineSet.rootsignature));
+	//assert(SUCCEEDED(result));
+	////パイプラインにルートシグネチャをセット
+	//pipelineDesc.pRootSignature = pipelineSet.rootsignature.Get();
+
+	////パイプラインステート (グラフィックスパイプラインの設定をまとめたのがパイプラインステートオブジェクト(PSO))
+	////パイプラインステートの生成
+	//result = dxCommon->GetDevice()->CreateGraphicsPipelineState(&pipelineDesc, IID_PPV_ARGS(&pipelineSet.pipelinestate));
+	//assert(SUCCEEDED(result));
 
 	//パイプラインとルートシグネチャを返す
-	return pipelineSet;
-}
+	//return pipelineSet;
+//}
 
 
 //3DObject共通グラフィックスコマンドのセット
