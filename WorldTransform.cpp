896ecaf179162578830ca1f66699805857e6c319
634_3 <k020g1146@g.neec.ac.jp>
2022-08-02 20:42:20 +0900

    WorldTransform 実装

--- WorldTransform.cpp
+++ WorldTransform.cpp
@@ -1,8 +1,9 @@
 ﻿#include "WorldTransform.h"
 #include "DirectXCommon.h"
-
 #include <cassert>
 #include <d3dx12.h>
+
+using namespace DirectX;
 
 void WorldTransform::Initialize()
 {
@@ -16,18 +17,50 @@ void WorldTransform::CreateConstBuffer()
 	HRESULT result = S_FALSE;
 
 	//ヒーププロパティ
-
+	CD3DX12_HEAP_PROPERTIES heapProp = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
 	//リソース設定
-
+	CD3DX12_RESOURCE_DESC resourceDesc = CD3DX12_RESOURCE_DESC::Buffer((sizeof(ConstBufferDataWorldTransform) + 0xff) & ~0xff);
+	
 	//定数バッファ生成
+	result = DirectXCommon::GetInstance()->GetDevice()->CreateCommittedResource
+		(
+			&heapProp, D3D12_HEAP_FLAG_NONE, 
+			&resourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr, IID_PPV_ARGS(&constBuff)
+		);
+	assert(SUCCEEDED(result));
 }
 
 void WorldTransform::Map()
 {
-
+	//定数バッファのデータリンク
+	HRESULT result = S_FALSE;
+	result = constBuff->Map(0, nullptr, (void**)&constMap);
+	assert(SUCCEEDED(result));
 }
 
 void WorldTransform::UpdateMatrix()
 {
+	XMMATRIX matScale, matRot, matTrans;
 
+	matScale = XMMatrixScaling(scale.x, scale.y, scale.z);
+	matRot = XMMatrixIdentity();
+	matRot *= XMMatrixRotationZ(rotation.z);
+	matRot *= XMMatrixRotationX(rotation.x);
+	matRot *= XMMatrixRotationY(rotation.y);
+	matTrans = XMMatrixTranslation(translation.x, translation.y, translation.z);
+
+	matWorld = XMMatrixIdentity();
+	matWorld *= matScale;
+	matWorld *= matRot;
+	matWorld *= matTrans;
+
+	//親行列
+	if(parent)
+	{
+		matWorld *= parent->matWorld;
+	}
+
+	//定数バッファ書き込み
+	constMap->matWorld = matWorld;
 }
