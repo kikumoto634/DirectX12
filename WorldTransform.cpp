e5e4ebef952be58d74c235259781a1554510b582
k020g1146 <k020g1146@g.neec.ac.jp>
2022-08-02 16:57:00 +0900

    WorldTransform 実装中

--- WorldTransform.cpp
+++ WorldTransform.cpp
@@ -1,1 +1,33 @@
 ﻿#include "WorldTransform.h"
+#include "DirectXCommon.h"
+
+#include <cassert>
+#include <d3dx12.h>
+
+void WorldTransform::Initialize()
+{
+	CreateConstBuffer();
+	Map();
+	UpdateMatrix();
+}
+
+void WorldTransform::CreateConstBuffer()
+{
+	HRESULT result = S_FALSE;
+
+	//ヒーププロパティ
+
+	//リソース設定
+
+	//定数バッファ生成
+}
+
+void WorldTransform::Map()
+{
+
+}
+
+void WorldTransform::UpdateMatrix()
+{
+
+}
